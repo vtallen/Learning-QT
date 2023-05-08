41e0b11cc540e30b4f874e18d3d446596ba23622
vtallen <vtallen@protonmail.com>
2023-05-08 16:50:03 -0400

    FInished the QDir lecture and made an application that lists the files in a dir.

---
+++ QDirDemo/build-QDirDemo-Desktop_Qt_6_5_0_GCC_64bit-Debug/QDirDemo_autogen/EWIEGA46WW/moc_widget.cpp
@@ -0,0 +1,183 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../QDirDemo/widget.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSWidgetENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSWidgetENDCLASS = QtMocHelpers::stringData(
+    "Widget",
+    "chooseDirButtonClicked",
+    "",
+    "createDirButtonClicked",
+    "dirExistsButtonClicked",
+    "dirOrFileButtonClicked",
+    "folderContentsButtonClicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSWidgetENDCLASS_t {
+    uint offsetsAndSizes[14];
+    char stringdata0[7];
+    char stringdata1[23];
+    char stringdata2[1];
+    char stringdata3[23];
+    char stringdata4[23];
+    char stringdata5[23];
+    char stringdata6[28];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSWidgetENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSWidgetENDCLASS_t qt_meta_stringdata_CLASSWidgetENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 6),  // "Widget"
+        QT_MOC_LITERAL(7, 22),  // "chooseDirButtonClicked"
+        QT_MOC_LITERAL(30, 0),  // ""
+        QT_MOC_LITERAL(31, 22),  // "createDirButtonClicked"
+        QT_MOC_LITERAL(54, 22),  // "dirExistsButtonClicked"
+        QT_MOC_LITERAL(77, 22),  // "dirOrFileButtonClicked"
+        QT_MOC_LITERAL(100, 27)   // "folderContentsButtonClicked"
+    },
+    "Widget",
+    "chooseDirButtonClicked",
+    "",
+    "createDirButtonClicked",
+    "dirExistsButtonClicked",
+    "dirOrFileButtonClicked",
+    "folderContentsButtonClicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSWidgetENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   44,    2, 0x08,    1 /* Private */,
+       3,    0,   45,    2, 0x08,    2 /* Private */,
+       4,    0,   46,    2, 0x08,    3 /* Private */,
+       5,    0,   47,    2, 0x08,    4 /* Private */,
+       6,    0,   48,    2, 0x08,    5 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject Widget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_CLASSWidgetENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSWidgetENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSWidgetENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<Widget, std::true_type>,
+        // method 'chooseDirButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'createDirButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'dirExistsButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'dirOrFileButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'folderContentsButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Widget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->chooseDirButtonClicked(); break;
+        case 1: _t->createDirButtonClicked(); break;
+        case 2: _t->dirExistsButtonClicked(); break;
+        case 3: _t->dirOrFileButtonClicked(); break;
+        case 4: _t->folderContentsButtonClicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSWidgetENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 5;
+    }
+    return _id;
+}
+QT_WARNING_POP
